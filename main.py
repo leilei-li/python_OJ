37b2f7510dd98cb6f5740d0265f8171b7f95465f
leilei-li <lileilei258@qq.com>
2018-05-30 18:26:19 +0800

    update ReadMe

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from leetcode_1 import Solution
 def main():
     s = Solution()
     # s.findMedianSortedArrays([1, 2], [3,4])
-    print(s.maxArea([14,0,12,3,8,3,13,5,14,8]))
+    print(s.maxArea([14, 0, 12, 3, 8, 3, 13, 5, 14, 8]))
 
 
 if __name__ == '__main__':
     main()
-
