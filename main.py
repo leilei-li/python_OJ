cb9fc12d0407a60eb9fe69d7053d440653f1adae
leilei-li <lileilei258@qq.com>
2018-06-01 17:55:08 +0800

    threeSum

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from leetcode_1 import Solution
 def main():
     s = Solution()
     # s.findMedianSortedArrays([1, 2], [3,4])
-    print(s.longestCommonPrefix(["dog","racecar","car"]))
+    print(s.threeSum([-1, 0, 1, 2, -1, -4]))
 
 
 if __name__ == '__main__':
