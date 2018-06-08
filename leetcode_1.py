a0443f616c72809749b7be066d91e623a1f67c1f
leilei-li <lileilei258@qq.com>
2018-06-08 23:20:41 +0800

    searchInsert

--- leetcode_1.py
+++ leetcode_1.py
@@ -860,3 +860,19 @@ class Solution:
             if nums[i] == target:
                 end = i
         return [start, end]
+
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if nums[0] > target:
+            return 0
+        if nums[-1] < target:
+            return len(nums)
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+            if nums[i] < target and nums[i + 1] > target:
+                return i + 1
