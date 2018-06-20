4bd89b8035909eac768fa927c4ec7e8481c476a1
leilei-li <lileilei258@qq.com>
2018-06-20 16:05:25 +0800

    rotate

--- leetcode_1.py
+++ leetcode_1.py
@@ -1105,3 +1105,17 @@ class Solution:
             if self.nums[i] == self.nums[end]:
                 return True
         return False
+
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(int(n / 2)):
+            for j in range(i, n - 1 - i):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[n - j - 1][i]
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+                matrix[j][n - i - 1] = temp
